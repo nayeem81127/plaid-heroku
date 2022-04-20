e17cbb2c2e118d46a0893c779a9d21e20efd707b
unknown <nayeem.uddin@axolt.com>
2022-04-20 12:02:45 +0530

    make it better

--- node/index.js
+++ node/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
-const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+const PLAID_ENV = process.env.PLAID_ENV || 'development';
 
 // PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 // Link. Note that this list must contain 'assets' in order for the app to be
