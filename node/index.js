4584bc83e5fbe2b0ed5ff19da05138eceda97851
unknown <nayeem.uddin@axolt.com>
2022-05-05 23:38:31 +0530

    make it better

--- node/index.js
+++ node/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
-const PLAID_ENV = process.env.PLAID_ENV || 'development';
+const PLAID_ENV = process.env.PLAID_ENV || 'production';
 
 // PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 // Link. Note that this list must contain 'assets' in order for the app to be
@@ -23,7 +23,7 @@ const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || 'transactions').split(
 
 // PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
 // will be able to select institutions from.
-const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'CA').split(
   ',',
 );
 
